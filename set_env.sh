8e836e88eae0f6f7c0debc6617d04b09be6544e8
Tan Nguyen <ngtrantan99@gmail.com>
2023-11-09 19:02:00 +0700

    test

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=12345678
-export POSTGRES_HOST=database-1.czj3xkhbtyce.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=mypassword
+export POSTGRES_HOST=postgres.cuq48cmrumr1.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=udacity-project3-215718144426-tinpb
+export AWS_BUCKET=arn:aws:s3:::ngtrantan
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
